7dae19374d8ae77363128785b7b1d6716c29a73f
Lee chae min <sklove96@naver.com>
2020-09-07 22:08:57 +0900

    'login'

---
+++ routes/auth.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+
+const router = express.Router();
+
+router.post('/join', isNotLoggedIn, async (req, res, next) => { //회원가입
+    const { email, nick, password } = req.body;
+    try {
+      const exUser = await User.findOne({ where: { email } });
+      if (exUser) { // 이메일로 가입한 사용자 존재하면
+        return res.redirect('/join?error=exist');
+      }
+      const hash = await bcrypt.hash(password, 12); //암호화
+      await User.create({
+        email,
+        nick,
+        password: hash,
+      });
+      return res.redirect('/');
+    } catch (error) {
+      console.error(error);
+      return next(error);
+    }
+  });
+  
+  router.post('/login', isNotLoggedIn, (req, res, next) => { //로그인
+    passport.authenticate('local', (authError, user, info) => {
+      if (authError) {
+        console.error(authError);
+        return next(authError);
+      }
+      if (!user) {
+        return res.redirect(`/?loginError=${info.message}`);
+      }
+      return req.login(user, (loginError) => {
+        if (loginError) {
+          console.error(loginError);
+          return next(loginError);
+        }
+        return res.redirect('/');
+      });
+    })(req, res, next); // 미들웨어 내의 미들웨어에는 (req, res, next)를 붙입니다.
+  });
+  
+  router.get('/logout', isLoggedIn, (req, res) => { //로그아웃
+    req.logout();
+    req.session.destroy(); //req.session의 정보 제거
+    res.redirect('/');
+  });
+  
+  module.exports = router;
