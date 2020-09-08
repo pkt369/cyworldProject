d575cbc2bfaf5ea7d494abe7443e64576023d757
sejun <pkt369@naver.com>
2020-09-08 22:47:34 +0900

    sejun0.0.1

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const http = require('http');
-const fs = require('fs').promises;
+
 const path = require('path');
-
 const express = require('express');
-
 const nunjucks = require('nunjucks');
-
 const passport = require('passport');
-
 const dotenv = require('dotenv');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+
 dotenv.config();
 
 const pageRouter = require('./routes/page');
@@ -20,6 +19,20 @@ const app = express();
 app.set('port', process.env.PORT || 8001);
 
 //app.use(express.static('views'));
+// app.use(express.static('images'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}));
 
 //app.get('/',function(req,res){
 //  res.send('<img src="a.jpg" alt="no image" width="250" height="180" >');
@@ -61,27 +74,7 @@ app.use('/auth', authRouter);
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
 });
-//app.listen(3000,function(){
-//  console.log("포트 3000에서 응답함");
-//  //포트 3000에 연결이 된 경우 실행하는 콜백함수.
-//});
- 
 
 
 
-
-//  http.createServer(async (req, res) => {
-//    try {
-//      const data = await fs.readFile('views/home.html');
-//      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-//      res.end(data);
-//    } catch (err) {
-//      console.error(err);
-//      res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
-//      res.end(err.message);
-//    }
-//  })
-//    .listen(8081, () => {
-//      console.log('8081번 포트에서 서버 대기 중입니다!');
-//    });
 
---
+++ routes/home_image.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/images', (req, res) => {
+    res.render
+});
+
+module.exports = router;
