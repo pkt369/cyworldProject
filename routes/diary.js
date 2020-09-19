00c59f50c2f2012786033c01b6a033365626fb35
sejun <pkt369@naver.com>
2020-09-19 19:37:38 +0900

    sejunAllChange0.0.4

--- routes/diary.js
+++ routes/diary.js
@@ -28,11 +28,12 @@ const upload = multer({
     limits: { fileSize: 5 * 1024 * 1024 },
 });
 
-router.post('/upload', upload.single('image'), (req, res) => {
+router.post('/:id/image', upload.single('image'), (req, res) => {
     console.log('ㅎㅇㅎㅇ');
     console.log(req.file, req.body);
     res.send('ok');
-    //res.json({ url: `/image/${req.file.filename }`});
+    res.json({ url: `/image/${req.file.filename }`});
+    
 });
 
 // const upload2 = multer();
@@ -44,19 +45,73 @@ router.post('/upload', upload.single('image'), (req, res) => {
 //     }
 // })
 
+
 router.use((req, res, next) => {
-    res.locals.user = req.user; //유저의 정보를 넘겨주기
+    //res.locals.user = req.user; //유저의 정보를 넘겨주기
     next();
 });
 
-router.get('/', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
     try {
-        res.render('diaryBase');
+      const user = await User.findOne({ where: { email: req.params.id } });
+    if (user) {
+      await res.render('diary_home', { user : user }); //넘겨줄때 데이터가 존재하면 여기로 들어오고 들어온다면 그정보를 넘겨주기.
+    } else {
+      res.status(404).send('no user');
+    }
+        
     } catch (error) {
         console.error(error);
         next(err);
     } 
 });
+
+router.get('/:id/diary', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { email: req.params.id } });
+  if (user) {
+    await res.render('diary_diary', { user : user }); //넘겨줄때 데이터가 존재하면 여기로 들어오고 들어온다면 그정보를 넘겨주기.
+  } else {
+    res.status(404).send('no user');
+  }
+      
+  } catch (error) {
+      console.error(error);
+      next(err);
+  } 
+});
+
+router.get('/:id/guestbook', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { email: req.params.id } });
+  if (user) {
+    await res.render('diary_guestbook', { user : user }); //넘겨줄때 데이터가 존재하면 여기로 들어오고 들어온다면 그정보를 넘겨주기.
+  } else {
+    res.status(404).send('no user');
+  }
+      
+  } catch (error) {
+      console.error(error);
+      next(err);
+  } 
+});
+
+router.get('/:id/images', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { email: req.params.id } });
+  if (user) {
+    await res.render('diary_images', { user : user }); //넘겨줄때 데이터가 존재하면 여기로 들어오고 들어온다면 그정보를 넘겨주기.
+  } else {
+    res.status(404).send('no user');
+  }
+      
+  } catch (error) {
+      console.error(error);
+      next(err);
+  } 
+});
+
+
 
 // router.get('/upload', (req, res) => {
 //     res.sendFile(path.join(__dirname))
